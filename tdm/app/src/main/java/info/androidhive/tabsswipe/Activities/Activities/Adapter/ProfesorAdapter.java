d53eb87e4fa6ae186a652a652a16efea2bbdd419
Facundo Suarez <Suarezfacundon@gmail.com>
2017-11-02 21:12:33 -0300

    SearchProfesor Funcionando y otros cambios

--- tdm/app/src/main/java/info/androidhive/tabsswipe/Activities/Activities/Adapter/ProfesorAdapter.java
+++ tdm/app/src/main/java/info/androidhive/tabsswipe/Activities/Activities/Adapter/ProfesorAdapter.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Filter;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -17,6 +18,7 @@ import info.androidhive.tabsswipe.Activities.Entities.Comentario;
 import info.androidhive.tabsswipe.Activities.Entities.Profesor;
 import info.androidhive.tabsswipe.R;
 
+import java.util.Locale;
 /**
  * Created by USUARIO on 10/10/2017.
  */
@@ -26,11 +28,21 @@ public class ProfesorAdapter extends BaseAdapter {
     private ArrayList<Profesor> _profesores;
     private LayoutInflater _inflater;
     private Context _context;
+    private List<Profesor> _proflist = null;
+   /* private ValueFilter valueFilter;*/
+    ArrayList<Profesor> mOriginalValues;
 
     public ProfesorAdapter(Context context) {
         _profesores = new ArrayList<Profesor>();
         _inflater = LayoutInflater.from(context);
         _context=context;
+    }
+    public ProfesorAdapter(Context context,List<Profesor> proflist ) {
+        this._proflist = proflist;
+        _profesores = new ArrayList<Profesor>();
+        _inflater = LayoutInflater.from(context);
+        this._profesores.addAll(proflist);
+        getFilter();
     }
 
     public void setProfesores(Set<Profesor> profesores) {
@@ -44,6 +56,67 @@ public class ProfesorAdapter extends BaseAdapter {
     public Profesor getProfesores(int position) {
         Profesor profesor = (Profesor) getItem(position);
         return profesor;
+    }
+
+    public Filter getFilter()
+    {
+        Filter filter = new Filter()
+        {
+
+            @SuppressWarnings("unchecked")
+            @Override
+            protected void publishResults(CharSequence constraint,FilterResults results)
+            {
+
+                _profesores = (ArrayList<Profesor>) results.values; // has the filtered values
+                notifyDataSetChanged();  // notifies the data with new filtered values
+            }
+
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint)
+            {
+                FilterResults results = new FilterResults();        // Holds the results of a filtering operation in values
+                ArrayList<Profesor> FilteredArrList = new ArrayList<Profesor>();
+
+                if (mOriginalValues == null)
+                {
+                    System.out.println("");
+                    mOriginalValues = new ArrayList<Profesor>(_profesores); // saves the original data in mOriginalValues
+                }
+
+                /********
+                 *
+                 *  If constraint(CharSequence that is received) is null returns the mOriginalValues(Original) values
+                 *  else does the Filtering and returns FilteredArrList(Filtered)
+                 *
+                 ********/
+                if (constraint == null || constraint.length() == 0)
+                {
+
+                    // set the Original result to return
+                    results.count = mOriginalValues.size();
+                    results.values = mOriginalValues;
+
+                }
+                else
+                {
+                    constraint = constraint.toString().toLowerCase();
+                    for (int i = 0; i < mOriginalValues.size(); i++)
+                    {
+                        Profesor data = mOriginalValues.get(i);
+                        if (data.getApellido().toLowerCase().startsWith(constraint.toString()))
+                        {
+                            FilteredArrList.add(data);
+                        }
+                    }
+                    // set the Filtered result to return
+                    results.count = FilteredArrList.size();
+                    results.values = FilteredArrList;
+                }
+                return results;
+            }
+        };
+        return filter;
     }
 
     @Override
@@ -101,5 +174,19 @@ public class ProfesorAdapter extends BaseAdapter {
         TextView txtPuntuacion;
         //ImageView iconBigPopulation;
         TextView txtNombreProfesor;
+    }
+    public void filter(String charText) {
+        charText = charText.toLowerCase(Locale.getDefault());
+        _proflist.clear();
+        if (charText.length() == 0) {
+            _proflist.addAll(_profesores);
+        } else {
+            for (Profesor wp : _profesores) {
+                if (wp.getNombre().toLowerCase(Locale.getDefault()).contains(charText)) {
+                    _proflist.add(wp);
+                }
+            }
+        }
+        notifyDataSetChanged();
     }
 }
--- tdm/app/src/main/java/info/androidhive/tabsswipe/Activities/Activities/SearchFragment.java
+++ tdm/app/src/main/java/info/androidhive/tabsswipe/Activities/Activities/SearchFragment.java
@@ -1,20 +1,95 @@
 package info.androidhive.tabsswipe.Activities.Activities;
 
-import info.androidhive.tabsswipe.R;
+import android.support.v4.app.ListFragment;
+
+import android.content.Context;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.TextView;
 
-public class SearchFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.List;
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
+import info.androidhive.tabsswipe.Activities.Activities.Adapter.ProfesorAdapter;
+import info.androidhive.tabsswipe.Activities.Dao.ProfesorDao;
+import info.androidhive.tabsswipe.Activities.Entities.Profesor;
+import info.androidhive.tabsswipe.R;
 
-		View rootView = inflater.inflate(R.layout.fragment_search, container, false);
-		
-		return rootView;
-	}
+public class SearchFragment extends ListFragment  {
+
+    ListView list;
+    ProfesorAdapter adapter;
+    EditText editsearch;
+    ArrayList<Profesor> arraylist = new ArrayList<Profesor>();
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return  inflater.inflate(R.layout.fragment_search, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        super.onCreate(savedInstanceState);
+
+        // Generate sample data
+
+        /*animalNameList = new String[]{"Lion", "Tiger", "Dog",
+                "Cat", "Tortoise", "Rat", "Elephant", "Fox",
+                "Cow","Donkey","Monkey"};*/
+
+        // Locate the ListView in listview_main.xml
+        /*list = (ListView) getListView().findViewById(R.id.)*/
+
+        /*ProfesorDao profesorDao= new ProfesorDao(getActivity());
+        List<Profesor> listaProfesores = profesorDao.obtenerProfesores();*/
+
+
+
+        getListView().setEmptyView(getView().findViewById(R.id.empty2));
+        // Locate the EditText in listview_main.xml
+        editsearch = (EditText) getView().findViewById(R.id.search);
+        editsearch.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void onTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {
+                ProfesorDao profesorDao= new ProfesorDao(getActivity());
+                List<Profesor> listaProfesores = profesorDao.obtenerProfesores();
+                adapter = new ProfesorAdapter(getActivity(),listaProfesores);
+                setListAdapter(adapter);
+                adapter.getFilter().filter(arg0);
+                getListView().setEmptyView(getView().findViewById(R.id.empty1));
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
+                                          int arg3) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable arg0) {
+                if (arg0 == null || arg0.length() == 0)
+                {
+                    getListView().setEmptyView(getView().findViewById(R.id.empty2));
+                   setListAdapter(null);
+
+                }
+
+            }
+        });
+    }
+
 }

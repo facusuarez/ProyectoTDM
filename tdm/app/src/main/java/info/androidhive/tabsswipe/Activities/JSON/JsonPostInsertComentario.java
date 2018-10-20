45c3779257827c8921b789c2185858fc4c6bba57
878076 <romina.tita@aa.com>
2018-10-20 11:16:30 -0300

    Refactor gradle and volley

--- tdm/app/src/main/java/info/androidhive/tabsswipe/Activities/JSON/JsonPostInsertComentario.java
+++ tdm/app/src/main/java/info/androidhive/tabsswipe/Activities/JSON/JsonPostInsertComentario.java
@@ -5,12 +5,13 @@ import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.Toast;
 
+
 import com.android.volley.Request;
+import com.android.volley.toolbox.Volley;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 
 import java.util.HashMap;
 import java.util.Map;
